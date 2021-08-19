3c79a59f8c26d8a8c55fe02146499b6c84fe4acb
shiory602 <suzushioufo@gmail.com>
2021-08-19 13:11:26 -0700

    change filename and delete a img

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import './styles/reset.css';
 import './styles/main.css';
 
-import Home from './components/home';
-import Login from './components/login';
-import Register from './components/register';
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
 
 ReactDOM.render(
   <React.StrictMode>
