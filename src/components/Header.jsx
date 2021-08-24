dcb96a6872a7abab6cc55984ae73f66982754ab7
shiory602 <suzushioufo@gmail.com>
2021-08-24 11:45:52 -0700

    delete images and edit header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,63 @@
+import '../styles/header.scss';
+import logo from '../images/newblue.svg';
+import { Link } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+
 const Header = () => {
   return (
     <>
-      <h2>header</h2>
+      <div className='Header'>
+        <nav>
+          <div className='Header-logo'>
+            <Link to='/'>
+              <img src={logo} alt='logo' />
+            </Link>
+          </div>
+          <div className='navbar'>
+            <ul>
+              <li>
+                <Link to='/about'>
+                  How We Work
+                </Link>
+              </li>
+              <li>
+                <Link to='/hire'>
+                  Hire Designers
+                </Link>
+              </li>
+              <li>
+                <Link to='pricing'>
+                  Pricing
+                </Link>
+              </li>
+              <li>
+                <Link to='/faqs'>
+                  FAQs
+                </Link>
+              </li>
+              <li>
+                <Link to='blog'>
+                  Blog
+                </Link>
+              </li>
+              <li>
+                <Link to='/apply'>
+                  <Button variant="outlined" color="primary">
+                    Apply to Join
+                  </Button>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className='nav-button'>
+            <Link to='/apply'>
+              <Button variant="contained" color="primary">
+                Hire Now
+              </Button>
+            </Link>
+          </div>
+        </nav>
+      </div>
     </>
   )
 }
